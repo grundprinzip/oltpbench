a37c30f139acdbf9ad2662853d874396e16be0fc
grundprinzip <grundprinzip@gmail.com>
2013-12-06 15:05:52 +0100

    More genial stuff

--- run/runHyrise.sh
+++ run/runHyrise.sh
@@ -1,2 +1,2 @@
-java -Dlog4j.configuration=log4j.properties -Xmx4G -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b hyrise -c config/simple_config_hyrise.xml  --execute true
+java -Dlog4j.configuration=log4j.properties -Xmx4G -cp `run/classpath.sh` com.oltpbenchmark.DBWorkload -b hyrise -c config/simple_config_hyrise.xml  --execute true -s 1 -o hyrise
 
