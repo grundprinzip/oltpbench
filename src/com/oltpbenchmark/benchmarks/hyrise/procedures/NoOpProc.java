829a56b27a5ea3896d0a818a7199d599d0e82a2a
grundprinzip <grundprinzip@gmail.com>
2013-12-10 11:32:47 +0100

    Adding HYRISE JDBC driver and updating the Stored Procedure sample

--- src/com/oltpbenchmark/benchmarks/hyrise/procedures/NoOpProc.java
+++ src/com/oltpbenchmark/benchmarks/hyrise/procedures/NoOpProc.java
@@ -9,7 +9,7 @@ import com.oltpbenchmark.api.SQLStmt;
 
 public class NoOpProc extends Procedure {
 
-    public final static SQLStmt NOP = new SQLStmt("{\"priority\":10,\"performance\": false,\"operators\":{\"0\":{\"type\": \"NoOp\"}},\"edges\": [[\"0\", \"0\"]]}");
+    public final static SQLStmt NOP = new SQLStmt("{\"priority\":1,\"performance\": false,\"operators\":{\"0\":{\"type\": \"NoOp\"}},\"edges\": [[\"0\", \"0\"]]}");
     
     public void run(Connection conn, int id) throws SQLException {
         PreparedStatement stmt = this.getPreparedStatement(conn, NOP);
